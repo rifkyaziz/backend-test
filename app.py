af944aaa2cece2c6e417755816ea456f13874176
rifkyaziz <rifkyaziz@yandex.com>
2018-12-16 19:25:16 +0700

    add jwt, change test, and add endpoint

--- app.py
+++ app.py
@@ -1,44 +1,142 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask.ext.bcrypt import Bcrypt
-
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 import config
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
-db_uri = 'mysql://{user}:{password}@{host}:{port}/{dbname}?useSSL=false'.format(user = config.DATABASE_CONFIG['user'],
+db_uri = 'mysql://{user}:{password}@{host}:{port}/{dbname}'.format(user = config.DATABASE_CONFIG['user'],
                                                                                     password = config.DATABASE_CONFIG['password'],
                                                                                     host = config.DATABASE_CONFIG['host'],
                                                                                     port = config.DATABASE_CONFIG['port'],
                                                                                     dbname = config.DATABASE_CONFIG['dbname'])
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'very-secure'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+jwt = JWTManager(app)
 
 # User Model
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True)
     password = db.Column(db.String(128))
-    products = relationship("Product")
+    products = db.relationship("Product")
 
-    def __init__(self, email, password, products):
+    def __init__(self, email, password):
         self.email = email
-        self.password = bcrypt(password)
+        self.password = bcrypt.generate_password_hash(password)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "password": self.password
+        }
 
 # Product Model
 class Product(db.Model):
+    __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
     size = db.Column(db.String(2))
-    user_id = Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-@app.route('/products')
-def get_all_products():
-    return "Hello, World"
+    def __init__(self, name, size, user_id):
+        self.name = name
+        self.size = size
+        self.user_id = user_id
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def get_user(self):
+        user = User.query.filter_by(id=self.user_id).first()
+        return user.serialize()
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "size": self.size,
+            "user": self.get_user()
+        }
+
+@app.route('/api/login', methods = ['POST'])
+def login():
+    req_data = request.get_json(force=True)
+    if not req_data:
+        return jsonify(message="Error, request data is not valid")
+
+    access_token = create_access_token(identity=req_data["email"])
+    return jsonify(access_token = access_token)
+
+@app.route('/api/products', methods = ['POST'])
+@jwt_required
+def create_product():
+    req_data = request.get_json(force=True)
+    if not req_data:
+        return jsonify(message="Error, request data is not valid")
+
+    product = Product(req_data["name"],
+                      req_data["size"],
+                      req_data["user_id"])
+    product.save()
+    return jsonify(message="Successfully save data",
+                   data=product.serialize())
+
+@app.route('/api/products/<id>')
+@jwt_required
+def get_product(id):
+    product = Product.query.filter_by(id=id).first()
+    if not product:
+        return jsonify(message="Product not found", data = {})
+    
+    return jsonify(message="Successfully get data",
+                   data=product.serialize())
+
+@app.route('/api/products/<id>', methods = ['PUT'])
+@jwt_required
+def update_product(id):
+    req_data = request.get_json(force=True)
+    if not req_data:
+        return jsonify(message="Error, request data is not valid")
+    
+    product = Product.query.filter_by(id=id).first()
+    if not product:
+        return jsonify(message="Product not found", data = {})
+
+    product.name = req_data["name"]
+    product.size = req_data["size"]
+    product.user_id = req_data["user_id"]
+    db.session.commit()
+
+    return jsonify(message="Successfully update data",
+                   data=product.serialize())
+
+@app.route('/api/products/<id>', methods = ['DELETE'])
+@jwt_required
+def delete_product(id):
+    product = Product.query.filter_by(id=id).first()
+    if not product:
+        return jsonify(message="Product not found")
+    
+    db.session.delete(product)
+    db.session.commit()
+    return jsonify(message="Successfully delete data")
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
---
+++ test_rest_api.py
@@ -0,0 +1,46 @@
+import unittest
+
+from app import *
+import json
+
+class RestApiTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """Initialize test variables and app"""
+        self.client = app.test_client(self)
+        self.app = app
+        self.product_data = {
+            'name': 'Shirt', 'size': 'XL', 'user_id': 1
+        }
+        self.product_data_update = {
+            'name': 'Shirt Web', 'size': 'M', 'user_id': 1
+        }
+        with self.app.app_context():
+            db.create_all()
+
+    def test_01_create_product(self):
+        req = self.client.post('/api/products', json = self.product_data)
+        res = req.get_json()
+        self.assertEqual(req.status_code, 200)
+        self.assertEqual(res['message'], 'Successfully save data')
+
+    def test_02_get_product(self):
+        req = self.client.get('/api/products/1')
+        res = req.get_json()
+        self.assertEqual(req.status_code, 200)
+        self.assertEqual(res['message'], 'Successfully get data')
+
+    def test_03_update_product(self):
+        req = self.client.put('/api/products/1', json = self.product_data_update)
+        res = req.get_json()
+        self.assertEqual(req.status_code, 200)
+        self.assertEqual(res['message'], 'Successfully update data')
+
+    def test_04_delete_product(self):
+        req = self.client.delete('/api/products/1')
+        res = req.get_json()
+        self.assertEqual(req.status_code, 200)
+        self.assertEqual(res['message'], 'Successfully delete data')
+
+    if __name__ == "__main__":
+        unittest.main()
